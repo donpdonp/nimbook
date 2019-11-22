70484a21ab23ac4057b80737a2240c663f82492c
Don Park <donp@donp.org>
2019-11-22 10:58:09 -0800

    output cleanup

--- src/types/book.nim
+++ src/types/book.nim
@@ -12,8 +12,12 @@ type
     books*: seq[Book]
 
 proc `$`*(b: Book): string =
+  let lowidx = low(b.offers)
+  let highidx = high(b.offers)
   b.market.`$` & " " & len(b.offers).`$` & " offers " & (if len(b.offers) > 0:
-      b.offers[0].quote_qty.formatFloat(ffDecimal, 4) & " - " & b.offers[high(b.offers)].quote_qty.formatFloat(ffDecimal, 4) else: "")
+      b.offers[lowidx].`quote$` &
+      " - " &
+      b.offers[highidx].`quote$` else: "")
 
 proc `$`*(bs: Books): string =
   bs.askbid.`$` & " " & bs.books.map(proc (b:Book): string = b.`$`).join(" ")
--- src/types/offer.nim
+++ src/types/offer.nim
@@ -9,6 +9,9 @@ type
 proc `$`*(o: Offer): string =
   o.base_qty.formatFloat(ffDecimal, 4) & "@" & o.quote_qty.formatFloat(ffDecimal, 4)
 
+proc `quote$`*(o: Offer): string =
+  o.quote_qty.formatFloat(ffDecimal, 4)
+
 proc quote*(o: Offer, flipped: bool): float =
   if flipped:
     1/o.quote_qty

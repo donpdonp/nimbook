b3e2b14c03c3cee32d9a500dd7c579943900fcfc
Don Park <donp@donp.org>
2019-12-02 11:47:59 -0800

    setup some test data

--- tests/test_1.nim
+++ tests/test_1.nim
@@ -2,12 +2,20 @@ import types
 include nimbook, config
 
 proc t_1 =
-  var config = load("config.yaml")
+  var sourceA = Source(name:"Exchange A")
+  var marketA = Market(source: sourceA, base: "TKR1", quote: "TKR2")
+  var offersA = @[Offer(base_qty: 1, quote_qty: 2)]
+  var bookA = Book(market: marketA, offers: offersA)
 
-  echo "test 1"
-  var asks = Books(askbid: AskBid.ask)
-  var bids = Books(askbid: AskBid.bid)
+  var sourceB = Source(name:"Exchange B")
+  var marketB = Market(source: sourceB, base: "TKR1", quote: "TKR2")
+  var offersB = @[Offer(base_qty: 1, quote_qty: 1.1)]
+  var bookB = Book(market: marketB, offers: offersB)
+
+  var asks = Books(askbid: AskBid.ask, books: @[bookA])
+  var bids = Books(askbid: AskBid.bid, books: @[bookB])
   var (askwins, bidwins) = overlap(("TKR1", "TKR2"), asks, bids)
-  doAssert 0 == 0
+  doAssert 0 == len(askwins.books)
+  doAssert 0 == len(bidwins.books)
 
 t_1()

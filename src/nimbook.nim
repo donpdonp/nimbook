4de29f3d0be60c25e7601e0e00952caddfbb8211
Don Park <donp@donp.org>
2020-01-17 14:31:43 -0800

    restore profit calc

--- src/nimbook.nim
+++ src/nimbook.nim
@@ -4,7 +4,7 @@ import strformat, tables, sequtils
 # local
 import types, net
 
-proc trade*(askbooks: Books, bidbooks: Books): (Books, Books) =
+proc trade*(askbooks: Books, bidbooks: Books): (Books, Books, float) =
   if askbooks.askbid == AskBid.ask and bidbooks.askbid == Askbid.bid:
     # Sell to the asks, buy from the bids
     var asks_to_buy_from:Books
@@ -16,6 +16,7 @@ proc trade*(askbooks: Books, bidbooks: Books): (Books, Books) =
 
     let ask_orders = Books(askbid: AskBid.ask)
     let bid_orders = Books(askbid: AskBid.bid)
+    var profit:float
 
     for alist in asklist:
       for blist in bidlist:
@@ -24,13 +25,15 @@ proc trade*(askbooks: Books, bidbooks: Books): (Books, Books) =
           if qty > 0:
             alist[1].base_qty -= qty
             blist[1].base_qty -= qty
+            let price_diff = blist[1].quote - alist[1].quote
+            profit += qty * price_diff
             let buy_offer = Offer(base_qty: qty, quote: alist[1].quote)
-            let sell_offer = Offer(base_qty: qty, quote: blist[1].quote)
+            let sell_offer = Offer(base_qty: qty, quote: alist[1].quote)
             ask_orders.merge(alist[0], buy_offer)
             bid_orders.merge(blist[0], sell_offer)
     echo &"winning {ask_orders}"
     echo &"winning {bid_orders}"
-    (ask_orders, bid_orders)
+    (ask_orders, bid_orders, profit)
   else:
     raise newException(OSError, "askbooks bidbooks are not ask and bid!")
 
